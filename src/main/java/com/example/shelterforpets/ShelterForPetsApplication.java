25027d95426df10f457276ea581694087adccca0
Stanislav <stdu92@mail.ru>
2023-06-25 15:19:34 +0300

    Empty project

--- src/main/java/com/example/shelterforpets/ShelterForPetsApplication.java
+++ src/main/java/com/example/shelterforpets/ShelterForPetsApplication.java
@@ -2,7 +2,7 @@ package com.example.shelterforpets;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//пуш dev
 @SpringBootApplication
 public class ShelterForPetsApplication {
 
